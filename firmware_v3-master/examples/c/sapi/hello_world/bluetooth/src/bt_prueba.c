e53f0129f1984589b557f87c7070d0b30ec2e7bd
Nacho Torres <ntorresco@gmail.com>
2020-02-21 17:31:56 -0300

    21-2

--- firmware_v3-master/examples/c/sapi/hello_world/bluetooth/src/bt_prueba.c
+++ firmware_v3-master/examples/c/sapi/hello_world/bluetooth/src/bt_prueba.c
@@ -8,6 +8,7 @@
 #include "sapi.h"        // <= Biblioteca sAPI
 
 #define UART_BLUETOOTH UART_232
+#define UART_PC        UART_USB
 
 int main( void )
 {
@@ -19,18 +20,25 @@ int main( void )
    // Inicializar UART_232 para conectar al modulo bluetooth a 9600bps
    uartConfig( UART_BLUETOOTH, 9600 );
    uartWriteString(UART_BLUETOOTH, "AT+namePEPE\r\n");
-   printf("UART_BLUETOOTH para modulo Bluetooth configurada.\n" );
+   uartWriteString( UART_PC, "UART_BLUETOOTH para modulo Bluetooth configurada.\r\n" );
+  
+   uartConfig( UART_PC, 9600 );
+   uartWriteString( UART_PC, "UART_PC configurada.\r\n" );
+
 
    uint8_t data = 0;
 
-   if( hm10bleTest( UART_BLUETOOTH ) ){
+   /*if( hm10bleTest( UART_BLUETOOTH ) ){
         printf("Modulo conectado correctamente.\n" );
    }
    else{
         printf("No funciona.\n" );
-   }
+   }*/
 
    while(TRUE){
+	   if( uartReadByte( UART_PC, &data ) ) {
+         	uartWriteByte( UART_BLUETOOTH, data );
+   	   }
 	   if( uartReadByte( UART_BLUETOOTH, &data ) ) {
 	            if( data == 'h' ) {
 	               gpioWrite( LEDB, ON );
@@ -38,9 +46,27 @@ int main( void )
 	            if( data == 'l' ) {
 	               gpioWrite( LEDB, OFF );
 	            }
-	            printf( data );
-	            fflush(stdout);
-	         }
+	            uartWriteByte( UART_PC, data );
+	   }
+	   // Si presiono TEC1 imprime la lista de comandos AT
+	   if( !gpioRead( TEC1 ) ) {
+		 hm10blePrintATCommands( UART_BLUETOOTH );
+		 delay(500);
+	   }
    }
 
+}
+
+
+/*bool_t hm10bleTest( int32_t uart )
+{
+   uartWriteString( uart, "AT\r\n" );
+   return waitForReceiveStringOrTimeoutBlocking( uart,
+                                                 "OK\r\n", strlen("OK\r\n"),
+                                                 1000 );
+}
+*/
+void hm10blePrintATCommands( int32_t uart )
+{
+   uartWriteString( uart, "AT+HELP?\r\n" );
 }
